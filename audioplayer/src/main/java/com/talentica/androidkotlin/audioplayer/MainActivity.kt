82c3fd57c60331b64b539d841ac8c1d0e6608680
Dinkar Kumar <dinkar19121991@gmail.com>
2021-07-06 10:42:54 +0530

    updated the main audioplayer module to latest dependencies and also migrated the project to androidx, compiled and target SDK to 30

--- audioplayer/src/main/java/com/talentica/androidkotlin/audioplayer/MainActivity.kt
+++ audioplayer/src/main/java/com/talentica/androidkotlin/audioplayer/MainActivity.kt
@@ -3,17 +3,16 @@ package com.talentica.androidkotlin.audioplayer
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.PowerManager
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.talentica.androidkotlin.audioplayer.utils.LogHelper
 import com.talentica.androidkotlin.audioplayer.utils.SeekBarHandler
-
 
 
 class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener,
