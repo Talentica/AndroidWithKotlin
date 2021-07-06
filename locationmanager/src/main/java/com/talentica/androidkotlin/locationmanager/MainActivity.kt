166de15c12ce712be3d147f30500604e7b24e08f
Dinkar Kumar <dinkar19121991@gmail.com>
2021-07-06 11:39:59 +0530

    updated the main locationmanager module to latest dependencies and also migrated the project to androidx, compiled and target SDK to 30

--- locationmanager/src/main/java/com/talentica/androidkotlin/locationmanager/MainActivity.kt
+++ locationmanager/src/main/java/com/talentica/androidkotlin/locationmanager/MainActivity.kt
@@ -7,8 +7,8 @@ import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v7.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Button
